4adb2309494d7b75037df35bb068618d0c4bb530
bluehj888 <huajia@jlu.edu.cn>
2025-06-21 13:06:45 +0800

    Update update.sh
    Signed-off-by: bluehj888 <huajia@jlu.edu.cn>

--- update.sh
+++ update.sh
@@ -917,7 +917,7 @@ main() {
     support_fw4_adg
     update_script_priority
     fix_easytier
-    update_geoip
+    #update_geoip
     update_package "runc" "releases" "v1.2.6"
     update_package "containerd" "releases" "v1.7.27"
     update_package "docker" "tags" "v28.2.2"
